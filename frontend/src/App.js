14212e0fc71b973d55d6d91cba59864e02ed622f
vokpe <oso2006@nyu.edu>
2024-02-13 14:08:38 -0500

    Added containers for i/o
    adding more updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,17 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <div className="interpreter-container">
+        <div className="input-area">
+          {/* Area for user input */}
+        </div>
+        <div className="button-area">
+          {/* Buttons for import, save, etc. */}
+        </div>
+        <div className="output-area">
+          {/* Area to display output */}
+        </div>
+      </div>
       <Interpreter />
     </div>
   );
