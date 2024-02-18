943fec28a87b6e27cf9e9c9eca4f2c1ff720276f
Kelvin Sapathy <71108701+k-path@users.noreply.github.com>
2024-02-18 17:43:30 -0500

    added coderunner

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,13 @@ import React from 'react';
 import './App.css';
 import Navbar from './Components/Navbar';
 import Interpreter from './Interpreter/Interpreter';
+import CodeRunner from './CodeRunner';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <CodeRunner />
       <div className="interpreter-container">
         <div className="button-area">
           <button>IMPORT</button>
@@ -25,6 +27,7 @@ function App() {
       </div>
       <Interpreter />
     </div>
+  
   );
 }
 
