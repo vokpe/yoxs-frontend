0610cdd8d0cf9968f863b32f5baf67b04cba2771
Kelvin Sapathy <kjs10010@nyu.edu>
2024-04-01 22:24:59 -0400

    fixed user input code execution

--- frontend/src/Interpreter/InterpreterUI.jsx
+++ frontend/src/Interpreter/InterpreterUI.jsx
@@ -1,28 +1,55 @@
-// src/Interpreter/InterpreterUI.jsx
-import React from 'react';
-import Interpreter from './Interpreter';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { BACKEND_URL } from '../constants'; // Ensure this import is correct
 
 const InterpreterUI = () => {
-  // put UI state and other functions here
+  const [userCode, setUserCode] = useState('');
+  const [executionResult, setExecutionResult] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleUserCodeChange = (event) => {
+    setUserCode(event.target.value);
+  };
+
+  const executeUserCode = async () => {
+    setIsLoading(true);
+    try {
+      // Assuming the backend expects a POST request with the user code
+      const response = await axios.post(`${BACKEND_URL}/code/compile`, { code: userCode });
+      console.log(response.data)
+      setExecutionResult(response.data.output); // Adjust based on how your backend sends back the result
+    } catch (error) {
+      console.error("There was an error executing the code:", error);
+      setExecutionResult("Error executing code. Please try again.");
+    }
+    setIsLoading(false);
+  };
 
   return (
     <div className="interpreter-container">
       <div className="button-area">
+        {/* Assuming these buttons will have functionality added later */}
         <button>IMPORT</button>
         <button>SAVE</button>
-        <button>PARSE</button>
+        <button onClick={executeUserCode}>PARSE</button> {/* Updated to execute code */}
         <button>DEBUG</button>
       </div>
       USER INPUT HERE:
       <div className="input-area">
         <textarea 
+          value={userCode}
+          onChange={handleUserCodeChange}
           placeholder="Enter your code here" 
           style={{ width: '100%', height: '150px', marginTop: '10px', marginBottom: '10px' }}
         ></textarea>
       </div>
       OUTPUT:
       <div className="output-area">
-        <Interpreter />
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          <pre style={{ whiteSpace: 'pre-wrap', wordBreak: 'break-word' }}>{executionResult}</pre>
+        )}
       </div>
     </div>
   );
