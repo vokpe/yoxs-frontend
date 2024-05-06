06ccc1c1cdf5f3811863b7ec0a0d7e7082a98117
vokpe <oso2006@nyu.edu>
2024-05-05 23:11:47 -0400

    refine frontend for Home

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -13,22 +13,11 @@ function Home() {
     <div className="welcome-container">
       <h1>Welcome to Our Application</h1>
       <p>This is the place where you can run and test your code. Enjoy using our interpreter!</p>
-      <button onClick={navigateToInterpreter} style={buttonStyle}>
+      <button onClick={navigateToInterpreter} className="start-button">
         Start coding in Monkey now!
       </button>
     </div>
   );
 }
-
-const buttonStyle = {
-  padding: '10px 20px',
-  backgroundColor: '#4CAF50',
-  color: 'white',
-  border: 'none',
-  borderRadius: '5px',
-  cursor: 'pointer',
-  fontSize: '16px',
-  marginTop: '20px',
-};
 
 export default Home;
